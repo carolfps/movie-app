4d11375db3ee072d6db480bfa3c95c93fb62cdf7
carolfps <37373110+carolfps@users.noreply.github.com>
2020-07-12 18:30:22 -0300

    tratativa de erro no fetch

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import MovieDetail from './MovieDetail';
 const App = () => {
 
   const [movieList, setMovieList] = useState([]);
+  const [errorMsg, setErrorMsg] = useState('');
     useEffect(() => {
         async function fetchApi(){
             async function getApiData() {
@@ -17,7 +18,7 @@ const App = () => {
               for (let film of responseData) {
                 if (film.people.length > 0) {
                   const tokens = film.people[0].split("/");
-                  
+
                   if (tokens[tokens.length - 1] === "") {
                     film.people = [];
                   } else {
@@ -30,43 +31,49 @@ const App = () => {
               }
               return responseData;
             }
-
-            let previousValues = localStorage.getItem("@movie-app/movies");
-
-            if (previousValues) {              
-              setMovieList(JSON.parse(previousValues));
-
-              let responseData = await getApiData();
-              let responseDataStr = JSON.stringify(responseData);
-
-              if (previousValues !== responseDataStr) {
-                localStorage.setItem("@movie-app/movies", responseDataStr);
+            try{
+              let previousValues = localStorage.getItem("@movie-app/movies");
+  
+              if (previousValues) {              
+                setMovieList(JSON.parse(previousValues));
+  
+                let responseData = await getApiData();
+                let responseDataStr = JSON.stringify(responseData);
+  
+                if (previousValues !== responseDataStr) {
+                  localStorage.setItem("@movie-app/movies", responseDataStr);
+                  setMovieList(responseData);
+                }
+  
+              } else {
+                let responseData = await getApiData();
                 setMovieList(responseData);
+  
+                localStorage.setItem("@movie-app/movies", JSON.stringify(responseData));
               }
-
-            } else {
-              let responseData = await getApiData();
-              setMovieList(responseData);
-
-              localStorage.setItem("@movie-app/movies", JSON.stringify(responseData));
-            }
+            }catch (error){
+              setTimeout(() => setErrorMsg('Erro na busca de dados 😭'), 2000);
         };
-        fetchApi();
+      }
+      fetchApi();
     }, []);
 
   return (
-    <BrowserRouter>
-      <React.Fragment>
-        <Route exact path="/">
-          <h1>Filmes Studio Ghibli</h1>
-          {movieList.length > 0 && <MovieGrid movieList={movieList} />}
-        </Route>
-        <Route exact path="/:title" >
-          {movieList.length > 0 && <MovieDetail movieList={movieList} />}
-        </Route>
-      </React.Fragment>
-    </BrowserRouter>
-  );
+      (errorMsg !== '') ?
+      (<h1>{errorMsg}</h1>) :
+      (
+        <BrowserRouter>
+        <React.Fragment>
+          <Route exact path="/">
+            {movieList.length > 0 && <h1>Filmes Studio Ghibli</h1>}
+            {movieList.length > 0 && <MovieGrid movieList={movieList} />}
+          </Route>
+          <Route exact path="/:title" >
+            {movieList.length > 0 && <MovieDetail movieList={movieList} />}
+          </Route>
+        </React.Fragment>
+      </BrowserRouter>
+      ));
 }
 
 export default App;
